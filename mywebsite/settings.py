c89feb0df229b85ac0997ccfa89fa4d7f7bbf703
Kaku-g <rjjatinrawat9@gmail.com>
2021-05-05 14:35:25 +0530

    second commit

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,8 +130,11 @@ MEDIA_URL ='/images/'
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'staticfiles')
 ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 django_heroku.settings(locals())
